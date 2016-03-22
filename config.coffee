221b26154ef05b3553df3292ff9c66828bf136c7
Brendan Goodenough <brendan@goodenough.nz>
2016-03-22 18:10:57 +1300

    Replace config.js with config.coffee

---
+++ config.coffee
@@ -0,0 +1,5 @@
+config =
+  title: 'LetterDB'
+  port: 3000
+
+module.exports = config
--- config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var config = {};
-
-config.title = 'LetterDB';
-config.port = 3000;
-
-module.exports = config;
